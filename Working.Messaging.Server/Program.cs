d6aab2d4cfa82b2d357c0fcf1846a57f3a0c312a
李强 <liqiang@bingosoft.net>
2016-06-25 20:51:38 +0800

    init

---
+++ Working.Messaging.Server/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace Working.Messaging.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<MessageServer>(s =>
+                {
+                    s.ConstructUsing(name => new MessageServer());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+            });
+        }
+    }
+}
---
+++ Working.Messaging.Server/Session.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Working.Messaging.Server
+{
+    public class Session
+    {
+    }
+}
